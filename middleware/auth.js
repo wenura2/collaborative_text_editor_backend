7ebbdca12d7c8998ec349ac3fd51a08910352b34
Danaka99 <140838401+Danaka99@users.noreply.github.com>
2026-05-12 20:37:18 +0530

    add changes for render

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,4 @@
-const ErrorHandler = require('../utils/ErrorHandler')
+const ErrorHandler = require('../utils/errorHandler')
 const catchAsyncErrors = require('./catchAsyncErrors')
 const jwt = require("jsonwebtoken");
 
